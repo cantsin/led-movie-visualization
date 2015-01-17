409e0f11fb7ad818ce3ab8a4d904e07b0fbe22c4
James Tranovich <james@openhorizonlabs.com>
2015-01-16 20:04:39 -0800

    Play back movies.

--- static/js/lmv.js
+++ static/js/lmv.js
@@ -1,4 +1,5 @@
 /* jshint esnext:true */
+/* global document, setTimeout, PIXI */
 
 let width = 320;
 let height = 240;
@@ -7,17 +8,30 @@ let video = document.createElement('video');
 video.autoplay = 'autoplay';
 video.loop = 'loop';
 video.muted = 'muted';
+video.controls = true;
 
-let canvas = document.createElement('canvas');
-canvas.width = width;
-canvas.height = height;
+let canvas = $('#player');
+canvas.attr('width', width);
+canvas.attr('height', height);
 
-let context = canvas.getContext('2d');
+let context = canvas[0].getContext('2d');
+
+video.addEventListener("play", function() {
+  // add an callback to render video to canvas.
+  let callback = function() {
+    if(video.paused) {
+      return;
+    }
+    context.drawImage(video, 0, 0, width, height);
+    setTimeout(() => { callback(); }, 0);
+  };
+  callback();
+}, false);
 
 let stage = new PIXI.Stage('0xcccccc');
-let buff8 = new Uint8ClampedArray(width*height);
+let buff8 = new Uint8ClampedArray(width * height);
 
-$("a.movies").click(function(_) {
+$("a.movies").click(function() {
   let name = 'movies/' + $(this).data('movie');
   video.src = name;
 });
