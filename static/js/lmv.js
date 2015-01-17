c1aa14928e287a5e0e53b6f7c62eb4551b27cb9c
James Tranovich <james@openhorizonlabs.com>
2015-01-16 19:51:10 -0800

    Beginnings of movie support.

--- static/js/lmv.js
+++ static/js/lmv.js
@@ -16,3 +16,8 @@ let context = canvas.getContext('2d');
 
 let stage = new PIXI.Stage('0xcccccc');
 let buff8 = new Uint8ClampedArray(width*height);
+
+$("a.movies").click(function(_) {
+  let name = 'movies/' + $(this).data('movie');
+  video.src = name;
+});
