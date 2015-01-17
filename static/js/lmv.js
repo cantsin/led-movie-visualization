b80a7d97005c5851769b6367b37d7b33d8826f7f
James Tranovich <james@openhorizonlabs.com>
2015-01-16 19:28:06 -0800

    Added pixi.js.

---
+++ static/js/lmv.js
@@ -0,0 +1,18 @@
+/* jshint esnext:true */
+
+let width = 320;
+let height = 240;
+
+let video = document.createElement('video');
+video.autoplay = 'autoplay';
+video.loop = 'loop';
+video.muted = 'muted';
+
+let canvas = document.createElement('canvas');
+canvas.width = width;
+canvas.height = height;
+
+let context = canvas.getContext('2d');
+
+let stage = new PIXI.Stage('0xcccccc');
+let buff8 = new Uint8ClampedArray(width*height);
