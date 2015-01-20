78fd24d076e59bf2951acac062446d0ae84d6d7b
James Tranovich <jtranovich@gmail.com>
2015-01-20 13:09:08 -0800

    Backing store for video retrieval.

--- static/js/lmv.js
+++ static/js/lmv.js
@@ -1,8 +1,8 @@
 /* jshint esnext:true */
 /* global document, setTimeout, PIXI */
 
-let width = 320;
-let height = 240;
+let width = 640;
+let height = 400;
 
 let video = $('#video')[0];
 video.autoplay = 'autoplay';
@@ -11,7 +11,6 @@ video.muted = 'muted';
 video.controls = true;
 video.width = width;
 video.height = height;
-video.type = "video/mp4";
 
 let canvas = $('#player');
 canvas.attr('width', width);
@@ -22,16 +21,49 @@ context.fillStyle = '#000000';
 context.fillRect(0, 0, width, height);
 context.fillStyle = '#ffffff';
 context.font = 'bold 16px Arial';
-context.fillText('No video selected.', width/4, height/2);
+context.fillText('No video selected.', width/3, height/2);
+
+// hard-coded for now
+let led_width = 150;
+let led_height = 90;
+let gap = Math.floor(width/led_width/2);
+
+// off-screen buffer, of sorts
+let backing = document.createElement('canvas');
+backing.width = led_width;
+backing.height = led_height;
+let backing_context = backing.getContext('2d');
 
 video.addEventListener('play', function() {
+  context.fillStyle = '#000000';
+  context.fillRect(0, 0, width, height);
   // add an callback to render video to canvas.
   let callback = function() {
-    if(video.paused) {
-      console.log('video paused.');
+    if(video.paused || video.ended) {
       return;
     }
-    context.drawImage(video, 0, 0, width, height);
+
+    // draw video offscreen
+    backing_context.drawImage(video, 0, 0, led_width, led_height);
+    let data = backing_context.getImageData(0, 0, led_width, led_height).data;
+
+    // retrieve video pixels and display in a grid
+    var x_index = 20;
+    var y_index = 20;
+    for(let j=0; j<led_height; j++) {
+      for(let i=0; i<led_width; i++) {
+        let p = 4 * (j * led_width + i);
+        let r = data[p + 0];
+        let g = data[p + 1];
+        let b = data[p + 2];
+        context.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+        context.fillRect(x_index, y_index, gap, gap);
+        x_index += gap*2;
+      }
+      x_index = 20;
+      y_index += gap*2;
+    }
+
     setTimeout(() => { callback(); }, 0);
   };
   callback();
