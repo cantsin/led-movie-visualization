12c4a4e6a7e7cf2bbee9a5e7747ea201acdf1d93
James Tranovich <jtranovich@gmail.com>
2015-01-21 21:46:03 -0800

    Allow binding to random port.

--- app.py
+++ app.py
@@ -94,5 +94,9 @@ def static_proxy(path):
     return app.send_static_file(path)
 
 if __name__ == '__main__':
+    import sys
     app.debug = True
-    app.run()
+    port = 5000
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    app.run(port=port)
